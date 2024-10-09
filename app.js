d4cb1f2f8e9176c8d0ac4c9962dbbcbaf641fa4b
Aekayi <aekayinyowin@gmail.com>
2024-10-09 11:49:39 +0630

    My first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var Slider = new Swiper(".pricing-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 2.57,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+  },
+});
